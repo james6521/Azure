dfe7bd560c5e2f954565e415ca4b71cdb833924e
james6521 <49067631+james6521@users.noreply.github.com>
2022-07-01 09:09:02 +0100

    changed variable

--- tf/CreatResGroup.tf
+++ tf/CreatResGroup.tf
@@ -22,7 +22,7 @@ provider "azurerm" {
 }
 # Create a resource group if it doesn't exist
 resource "azurerm_resource_group" "myterraformgroup" {
-    name     = "${var.resource_name}"
+    name     = "jamestest"
     location = "eastus"
 
     tags = {
